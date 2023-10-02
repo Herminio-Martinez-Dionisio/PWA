fc2ff88c9011c25972e0fae4b66df322ac3f5d3e
Herminio-Martinez-Dionisio <1719110493@utectulancingo.edu.mx>
2023-10-02 02:33:33 +0000

    Prueba1

---
+++ sw.js
@@ -0,0 +1,25 @@
+// sw.js
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('my-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/styles.css',
+          '/manifest.json'
+          // Agrega aquí más archivos que quieras que se almacenen en caché
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+
+  
+  
